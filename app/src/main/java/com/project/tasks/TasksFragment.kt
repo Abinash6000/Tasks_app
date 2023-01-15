ebb79e467fdbc417d7e1e55c8cfb06945c3ef3a7
Abinash <abinashdash6000@gmail.com>
2023-01-15 20:58:58 +0530

    Update

--- app/src/main/java/com/project/tasks/TasksFragment.kt
+++ app/src/main/java/com/project/tasks/TasksFragment.kt
@@ -16,17 +16,12 @@ class TasksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
-        Log.d("Here","1")
         val application = requireNotNull(this.activity).application
-        Log.d("Here","2")
         val dao = TaskDatabase.getInstance(application).taskDao
-        Log.d("Here","3")
         val viewModelFactory = TasksViewModelFactory(dao)
-        Log.d("Here","4")
         val viewModel = ViewModelProvider(this, viewModelFactory).get(TasksViewModel::class.java)
-        Log.d("Here","5")
         binding.viewModel = viewModel
-        Log.d("Here","6")
+        binding.lifecycleOwner = viewLifecycleOwner
         return view
     }
 
